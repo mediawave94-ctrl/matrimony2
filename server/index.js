fbe5515803e91a35315d12a4b6df08a118ad8161
Madhanbabu <babusmadhan@gmail.com>
2026-02-02 14:44:44 +0530

    Initial contribution

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('public/uploads')); // Serve uploads
 
